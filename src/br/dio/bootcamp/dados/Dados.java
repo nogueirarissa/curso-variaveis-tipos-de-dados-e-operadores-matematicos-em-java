cfd69e8fb42b84d06143dc9def22a99e4ebaa85f
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-07-20 21:35:34 -0300

    Aprendendo sobre dados

---
+++ src/br/dio/bootcamp/dados/Dados.java
@@ -0,0 +1,64 @@
+package br.dio.bootcamp.dados;
+
+public class Dados {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000; informa numero muito maior que a capacidade de armazenamento do short
+
+        //int i1 = -10000000000; valor muito abaixo do valor suportado pelo int
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055000L;
+
+        //float f1 = 4.5; quando coloca o numero assim, o java identifica como double
+        float f2 = 10.68f;
+
+        char c1 = 'W';
+        //char c2 = 'Tw'; char utiliza apenas um caractere
+        char c3 = '\u0057';
+
+        double d1 = 85.69;
+        double d2 = 99.84d;
+
+        String st1 = "Fulano";
+        String st2 = "Ciclano";
+        String st3 = "ag dgb se se et t KNBJBJBB &*&%& 75894389";
+
+        //String dt1 = "09/02/1981"; trabalhar com data com dados primitivos e inadequado
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+
+        System.out.println(s1);
+
+        System.out.println(i2);
+
+        System.out.println(l1);
+        System.out.println(l2);
+
+        System.out.println(f2);
+
+        System.out.println(c1);
+        System.out.println(c3);
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+    }
+}
